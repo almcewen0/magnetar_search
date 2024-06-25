921a3bffb6fbdff10ec80458a01ac58b3d5464d4
almcewen0 <almcewen0@hotmail.com>
2024-06-25 09:38:12 -0400

    initializing repo

---
+++ src/search_catalog_functions.py
@@ -0,0 +1,89 @@
+from search_config import config
+from astropy.table import Table
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from search_convenience_functions import *
+import numpy as np
+
+
+
+def read_catalog(c_cut):
+    """
+    Read in the catalog and make cuts as described below.
+    """
+    cf = config()
+    cat = Table.read(cf.catalog_file)
+    s_cat = SkyCoord(np.array(cat['LII'])*u.deg, np.array(cat['BII'])*u.deg,frame='galactic')
+    
+    b_cut = 5*u.deg
+    
+    
+    # abridged version has the following changes:
+    #    - omitted detections outside of |b| <= 5 deg
+    #    - omitted the SmallWindow observations
+    #    - omitted sources with PN_8_CTS < c_cut (including sources without a PN_8_CTS measurement
+    #    - added column for Hui's IDs
+    #    - added column for SIMBAD associations (position matching within 2*uncertainty of 4XMM position)
+    #    - excluded sources with SUM_FLAG >= 3
+ 
+    
+    cat.add_column(cat['MJD_STOP'] - cat['MJD_START'],name='TOBS')
+    abr_cnd = np.array(abs(s_cat.b.to('deg')) <= b_cut) \
+            & np.array(~cat['PN_8_CTS'].mask) \
+            & np.array((cat['PN_8_CTS'] >= c_cut).data) \
+            & np.array(cat['PN_SUBMODE'] != 'PrimeSmallWindow') \
+            & np.array(cat['SUM_FLAG'] <= 3)
+    
+    cat_abr = cat[abr_cnd]
+    return cat,cat_abr
+    
+def add_SN_columns(subcat):
+    """
+    Function to calculate S/N for different combinations of bands in 4XMM.
+    Calculates S/N for each band individually, then (optionally) steps through each *contiguous* combination.
+    """
+    bands = [1,2,3,4,5,8]
+    if 'SN_1' not in subcat.columns:
+        [subcat.add_column(calc_joint_sn(subcat[f'PN_{i}_RATE'],
+                                         subcat[f'PN_{i}_RATE_ERR'],
+                                         subcat['TOBS']
+                                        ),name=f"SN_{i}") for i in bands]
+    colnames = np.array(subcat.colnames)[['SN_' in v for v in list(subcat.colnames)]]
+    return subcat,colnames
+
+def calc_joint_sn(rates,rate_errs,tobs):
+    if len(np.shape(rates)) > 1:
+        return np.sum(np.array(rates)*np.sqrt(tobs),axis=0)/np.sqrt(np.sum(np.array(rate_errs)),axis=0)
+    else:
+        return np.sum([np.array(rates)*np.sqrt(tobs)],axis=0)/np.sqrt(np.sum([np.array(rate_errs)],axis=0))
+
+def optimal_bands(ln):
+    """
+    Function that utilizes combine_factors() to return the best energy ranges for the search. This will (hopefully)
+    optimize the H power of a periodic signal in the data.
+    EDIT 041724: No longer utilizes combine_factors, and just uses band with highest S/N.
+    """
+
+    joint_bands = np.array(["1","2","3","4","5",'1-2','2-3','3-4','4-5',
+                            '1-2-3','2-3-4','3-4-5','1-2-3-4','2-3-4-5','1-2-3-4-5'])
+    exposure = ln['TOBS'] * u.day.to('s')
+    snrs = np.zeros(len(joint_bands))
+    for i,combination in enumerate(joint_bands):
+        bands_to_check = combination.split('-')
+        if len(bands_to_check) == 1:
+            rates  = ln[f'PN_{bands_to_check[0]}_RATE']
+            drates = ln[f'PN_{bands_to_check[0]}_RATE_ERR']
+        else:
+            rates  = np.sum([ln[f'PN_{b}_RATE'] for b in bands_to_check])
+            drates = np.sqrt(np.sum([ln[f'PN_{b}_RATE_ERR']**2 for b in bands_to_check]))
+        snrs[i] = np.sum(rates)*np.sqrt(exposure)/np.sqrt(np.sum(drates))
+    best_bandints = joint_bands[snrs == snrs.max()]
+    if '-' in best_bandints[0]:
+        bs = np.array(best_bandints[0].split('-')).astype(int)
+        if bs.min() <= 3 and bs.max() > 3:
+            b1 = '-'.join([str(b) for b in range(bs.min(),4)])
+            b2 = '-'.join([str(b) for b in range(4,bs.max()+1)])
+            best_bandints = [b1,b2]
+
+    return [ints_to_band(bb) for bb in best_bandints]
+
---
+++ src/search_pipeline.py
@@ -0,0 +1,321 @@
+import numpy as np
+import datetime
+import time
+import os
+import argparse
+from search_convenience_functions import *
+from search_catalog_functions import *
+from search_math_functions import *
+from search_config import config
+from search_processing_functions import *
+from astropy import units as u
+from astropy.io import fits
+from glob import glob
+
+
+def main():
+    # columns extracted from full catalog
+    cols = [
+        'OBS_ID',        'IAUNAME',       'RA',            'DEC',           
+        'SUM_FLAG',      'EP_EXTENT',     'EP_EXTENT_ML',  'PN_FLAG',
+        'PN_8_RATE',     'PN_1_RATE',     'PN_2_RATE',     'PN_3_RATE',     'PN_4_RATE',     'PN_5_RATE',
+        'PN_8_RATE_ERR', 'PN_1_RATE_ERR', 'PN_2_RATE_ERR', 'PN_3_RATE_ERR', 'PN_4_RATE_ERR', 'PN_5_RATE_ERR',
+        'PN_1_BG',       'PN_2_BG',       'PN_3_BG',       'PN_4_BG',       'PN_5_BG',       'TOBS'
+        ]
+
+    parser = argparse.ArgumentParser(description="Pipeline to search XMM data for periodic signals.")
+    parser.add_argument("-ids",
+                        "--idfile",
+                         help="Filename containing list of observation IDs to search",
+                        type=str,
+                        required=True)
+    parser.add_argument("-minP",
+                        "--minimumPeriod",
+                         help="Minimum candidate period to include in search, " + \
+                              "default = 1 s",
+                        default=1.0,
+                        type=float)
+    parser.add_argument("-maxP",
+                        "--maximumPeriod",
+                         help="Maximum candidate period to include in search, " + \
+                              "default = 20 s",
+                        default=20.0,
+                        type=float)
+    parser.add_argument("-nPs",
+                        "--numberPeriods",
+                         help="Number of candidate periods between minP and maxP to search, " + \
+                              "default is 10000",
+                        default=10000,
+                        type=float)
+    parser.add_argument("-sn",
+                        "--SNcutoff",
+                         help="Minimum source S/N to be searched, " + \
+                              "default is to only search the highest S/N band.",
+                        default=0.0,
+                        type=float)
+    parser.add_argument("-cc",
+                        "--Countscutoff",
+                         help='Minimum number of counts in 4XMM catalog for a source to be included, ' + \
+                              'default is 200',
+                        type=float,
+                        default=200)
+    parser.add_argument("-v",
+                        "--verbose",
+                         help="Flag to print out information to stdout, " + \
+                              "default is False",
+                        type=bool,
+                        default=False,
+                        action=argparse.BooleanOptionalAction)
+    parser.add_argument("-ow",
+                        "--overwrite",
+                         help="Overwrite previous run's data, " + \
+                              "default is False",
+                        type=bool,
+                        default=False,
+                        action=argparse.BooleanOptionalAction)
+    parser.add_argument("-hd",
+                        "--headdirectory",
+                        help="Head directory to write out all pipeline data/results, " + \
+                             "default is where it is run",
+                        default="",
+                        type=str)
+    parser.add_argument("-rm",
+                        "--cleanup",
+                        help="Flag to delete data files after processing completes, default is False",
+                        default=False,
+                        type=bool,
+                        action=argparse.BooleanOptionalAction)
+
+    args      = parser.parse_args()
+    ids       = np.loadtxt(args.idfile,dtype=str)
+    if np.shape(ids) == ():
+        ids = [ids.tolist()]
+    search_frequencies = np.logspace(np.log10(1/args.maximumPeriod),
+                                     np.log10(1/args.minimumPeriod),
+                                     args.numberPeriods)
+    head_dir  = args.headdirectory
+    if head_dir == '':
+        head_dir = os.getcwd()
+    sn_cut    = args.SNcutoff
+    c_cut     = args.Countscutoff
+    verbose   = args.verbose
+    overwrite = args.overwrite
+    cleanup = args.cleanup    
+
+    tD = datetime.datetime.now()
+    cf = config()
+    cat,cat_abr = read_catalog(c_cut) 
+    
+    if verbose:
+        os.putenv("SAS_VERBOSITY","1")
+    else:
+        os.putenv("SAS_VERBOSITY","0")
+    os.putenv("SAS_VERBOSITY","0")
+    
+    for i,obsID in enumerate(ids):
+        if os.getcwd() != head_dir:
+            cd(head_dir,verbose=verbose,logfile=None)
+        subcat = cat_abr[cols][cat_abr['OBS_ID'] == obsID]
+        if len(subcat) == 0:
+            report(f'WARNING: ID {obsID} not found in abridged catalog. Using full catalog instead.',
+                   verbose=verbose,logfile=None)
+            subcat = cat[cols][cat['OBS_ID'] == obsID] 
+        subcat,colnames = add_SN_columns(subcat)
+ 
+
+        # stripping out any sources which are not detected above cutoff in any band
+        subcat[np.any(np.array([subcat[c]>sn_cut for c in colnames]).T,axis=1)]
+        
+        # building directory structure for obsID and then downloading the data
+        srd,oid,logfile = build_directories(subcat,verbose=verbose)
+        report(f"Processing started at {time.strftime('%c')}",verbose=verbose,logfile=logfile)
+        ts = time.perf_counter()
+        cd(srd,verbose=verbose,logfile=logfile)
+        cut_srcs = os.path.join(oid,'cut_sources.reg')
+        noncand_reg = open(cut_srcs,'w')
+        noncand_reg.write('# Region file format: DS9 version 4.1\n' + \
+                          'global color=red dashlist=8 3 width=2 font="helvetica 3 normal roman" ' + \
+                          'select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1\n' + \
+                          'physical\n')
+        for cand in cat[cat['OBS_ID'] == obsID]:
+            if cand['IAUNAME'] not in subcat['IAUNAME']:
+                noncand_reg.write(f"j2000; circle({cand['RA']},{cand['DEC']}," + \
+                                  f"{np.max([6,cand['EP_EXTENT']])*u.arcsec.to('deg')}) " + \
+                                   "# text={" + cand['IAUNAME'].split()[-1] + "}\n")
+        noncand_reg.close()
+        
+        retcode = getXMMData(obsID,srd,oid,verbose=verbose,logfile=logfile,ts=ts)
+        
+        if retcode == 1:
+            report(f"ERROR: Failure in download of OID #{obsID}; odf/ directory is missing. " + \
+                    "Skipping this observation ID.",verbose=verbose,logfile=logfile,ts=ts)
+            continue
+            
+        analysis_dir = os.path.join(oid,f"analysis_{tD.year}{tD.month}{tD.day}")
+        make_dir(analysis_dir,verbose=verbose,logfile=logfile)
+        cd(analysis_dir,logfile=logfile)
+    
+        # create/read the event file for the observation and omit time intervals contaminated by proton flares
+        pnEvtFile = os.path.join(oid,f"evtFilePN_{obsID}_flBackCorr.fit")
+        if not os.path.exists(pnEvtFile) or overwrite:
+            try:
+                pnEvtFileRaw = glob(oid+'/odf/P*PN*EV*')[0]
+            except:
+                report(f"ERROR: Raw event file is missing - download may have failed. skipping this beam.",
+                       verbose=verbose,logfile=logfile)
+                report(f"Processing ended at {time.strftime('%c')}",verbose=verbose,logfile=logfile)
+                report(f"Total duration: {time.perf_counter() - ts} seconds",verbose=verbose,logfile=logfile)
+                report(f"Generated {len(glob(os.path.join(oid,'4XMM*','timingFiles','*pn_*fit')))} timing files",
+                       verbose=verbose,logfile=logfile)
+                logfile.close()
+                continue
+            with fits.open(pnEvtFileRaw) as hdulist:
+                tmp = open(os.path.join(oid,f'{obsID}_header.txt'),'w')
+                for c in hdulist[0].header.cards:
+                    if c[0] not in ['HISTORY','COMMENT']:
+                        tmp.write(f"{c[0]} : {c[1]} '{c[2]}'\n")
+                tmp.close()
+            report(f"{os.path.join(oid,f'{obsID}_header.txt')} written",logfile=logfile,verbose=verbose)
+            pnEvtFile = corrFlBackPN(pnEvtFileRaw,obsID,verbose=verbose,logfile=logfile,ts=ts)
+            pnEvtFile = mv(pnEvtFile,oid,verbose=verbose,logfile=logfile)
+    
+        
+        # point to attitude file for source removal step
+        attFile = oid+'/odf/P' + obsID + 'OBX000ATTTSR0000.FIT'
+
+        #execute(f"ds9 {oid}/{obsID}_pn02-12keV.im -region {cut_srcs} -log " + \
+        #         "-saveimage png {oid}/cut_srcs_pn02-12keV.png -exit",
+        #    verbose=verbose,logfile=None)
+
+    
+        for j,ln in enumerate(subcat):
+
+
+            srcName = ln['IAUNAME'].replace(' ','_').replace('+','p')
+
+            # we want to search in whatever energy range will maximize our signal for a given source
+            # this function attempts to determine that from each band's rate (as well as adjacent combinations)
+            bands_to_search = optimal_bands(ln) 
+            if len(bands_to_search) == 0:
+                report("ERROR: optimal_bands() has failed. This may be due to 0 cts/s rates. " + \
+                      f"Skipping {srcName}.",verbose=verbose,logfile=logfile)
+                continue
+
+            # need to create region files for the source and a background annulus
+    
+            pnSrcReg  = os.path.join(oid,srcName,f"src_{srcName}_{obsID}_pn.reg") 
+            pnBackReg = os.path.join(oid,srcName,f"back_{srcName}_{obsID}_pn.reg")
+            if not os.path.exists(pnSrcReg) or not os.path.exists(pnBackReg) or overwrite:
+
+                # convert extent from arcsec to deg, also fix small extent sources
+                extent = float([ln['EP_EXTENT'] if ln['EP_EXTENT']>0 else 20][0])*u.arcsec.to('deg')
+
+                # use the appropriate background file for the best band
+                im = [os.path.join(oid,
+                                 f"{obsID}{ints_to_imFilSuffix(band_to_ints(str(band)))}" 
+                                  ) for band in bands_to_search]
+
+                for image in im:
+                    if not os.path.exists(image):
+                        img    = crtImPN(pnEvtFile,obsID,
+                                      band_ints=[band_to_ints(str(imFilSuffix_to_band('_'+image.split('_')[-1])))],
+                                      verbose=verbose,logfile=logfile,ts=None)
+                        mv(img,image,verbose=verbose,logfile=logfile)
+                    expmap = make_expmap(pnEvtFile,image,attFile,obsID,oid,
+                                             verbose=verbose,logfile=logfile,ts=None)
+                
+
+                pnSrcReg, pnBackReg, pnSwiss = crtRegPN(im,
+                                               pnEvtFile,
+                                               attFile,
+                                               obsID,
+                                               srcName,
+                                               ln['RA'],
+                                               ln['DEC'],
+                                               extent,
+                                               oid,
+                                               cat,
+                                               verbose=verbose,
+                                               logfile=logfile,ts=ts)
+                if pnSrcReg is None or pnBackReg is None:
+                    report(f"WARNING: Radial profile for source {srcName} " + \
+                            "appears to be below the background value; skipping.",verbose=verbose,logfile=logfile)
+                    continue
+                cp(pnSrcReg,os.path.join(oid,srcName),verbose=verbose,logfile=logfile)
+                cp(pnBackReg,os.path.join(oid,srcName),verbose=verbose,logfile=logfile)
+            
+                
+            timdir = os.path.join(oid,srcName,'timingFiles')
+            make_dir(timdir,verbose=verbose,logfile=logfile)
+            crtTimPN(pnEvtFile,
+                     pnSwiss,
+                     obsID,
+                     srcName,
+                     pnSrcReg,
+                     pnBackReg,
+                     ln['RA'],
+                     ln['DEC'],
+                     bands_to_search,
+                     oid,
+                     verbose=verbose,
+                     overwrite=overwrite,
+                     logfile=logfile,ts=ts)
+            
+            # finally, conduct period search and store H test results 
+            # NOTE: since we are only using the highest S/N beams above but this globs for any .fit file, 
+            #  sources which had previous processing will be searched in all beams. This isn't a big deal, but 
+            #  may very slightly increase completion time.
+            for timFile in glob(os.path.join(oid,srcName,'timingFiles',f"{srcName}_{obsID}_pn_tim*keV.fit")):
+                sources_detected = do_period_search(timFile,
+                                                    srcName,
+                                                    oid,
+                                                    freqs=search_frequencies,
+                                                    write_file_bn=f"{timFile.rstrip('.fit')}",
+                                                    verbose=verbose,
+                                                    overwrite=True,
+                                                    logfile=logfile,
+                                                    ts=ts)
+                # for any detections, we make spectra for the source and background regions
+                # these are moved to 'specFiles' in the source folder
+                if sources_detected:
+                    specdir = os.path.join(oid,srcName,'specFiles')
+                    make_dir(specdir,verbose=verbose,logfile=logfile)
+                    if not spec_files_exist(os.path.join(specdir,srcName),
+                                            obsID,
+                                            verbose=verbose,
+                                            logfile=logfile) or overwrite:
+                        crtSpecPN(pnEvtFile,
+                                  pnSwiss,
+                                  obsID,
+                                  srcName,
+                                  pnSrcReg,
+                                  pnBackReg,
+                                  prefix='',
+                                  verbose=verbose,
+                                  logfile=logfile,ts=ts)
+                        command = f'mv *.pha *.arf *.rmf {specdir}'
+                        execute(command,verbose=verbose,logfile=logfile)
+    
+    r_str = ' ---------------------------------------------------------- \n' + \
+            ' Pipeline ran to completion. Please check output for errors \n' + \
+            ' ---------------------------------------------------------- '
+    report(r_str,verbose=verbose,logfile=logfile)
+    n_cand = len(glob(os.path.join(oid,'results','*png')))
+    if cleanup:
+        report("Removing raw data files",verbose=verbose,logfile=logfile)
+        if n_cand == 0:
+            execute(f"rm -rf {oid}/odf",verbose=verbose,logfile=logfile)
+            execute(f"rm -rf {oid}/analysis*",verbose=verbose,logfile=logfile)
+        execute(f"rm -rf {srd}/files_{obsID}.tar",verbose=verbose,logfile=logfile)
+    report(f"Processing ended at {time.strftime('%c')}",verbose=verbose,logfile=logfile)
+    report(f"Total duration: {time.perf_counter() - ts} seconds",verbose=verbose,logfile=logfile)
+    report(f"Generated {len(glob(os.path.join(oid,'4XMM*','timingFiles','*pn_*fit')))} " + \
+           f"timing files, found {n_cand} candidates",
+           verbose=verbose,logfile=logfile)
+    logfile.close()
+    if job_success(oid):
+        with open(cf.completed_file,'a') as fil:
+            fil.write(obsID+"\n")
+
+if __name__ == '__main__':
+    main()
