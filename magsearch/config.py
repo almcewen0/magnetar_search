3cbb7e8c7a38a08e2d2426228507e949a0da82ad
almcewen0 <almcewen0@hotmail.com>
2024-06-26 10:50:12 -0400

    typo fix

--- magsearch/config.py
+++ magsearch/config.py
@@ -3,11 +3,11 @@ class config():
         self.headdir = "/Users/G42719884/Documents/work/census/"
         self.completed_file = self.headdir + "completed_beams.txt"
         self.catalog_file = self.headdir + "4XMM_DR13cat_v1.0.fits"
-    self.defaults = {
-        'minimumPeriod' : 0.5,
-        'maximumPeriod' : 20,
-        'Countscutoff'  : 200,
-        'verbose'       : True,
-        'overwrite'     : False,
-        'cleanup'       : False
-    }
+        self.defaults = {
+            'minimumPeriod' : 0.5,
+            'maximumPeriod' : 20,
+            'Countscutoff'  : 200,
+            'verbose'       : True,
+            'overwrite'     : False,
+            'cleanup'       : False
+        }
