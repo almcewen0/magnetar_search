be2f131f2c838f6f7eba6f4f448ebc6af422aedf
almcewen0 <almcewen0@hotmail.com>
2025-06-19 11:36:13 -0400

    removed SAS prefix

--- magsearch/search_code/search_config.py
+++ magsearch/search_code/search_config.py
@@ -3,5 +3,4 @@ class config():
         self.headdir = "/Users/G42719884/repos/magnetar_search/magsearch/reference/"
         self.completed_file = self.headdir + "completed_beams.txt"
         self.catalog_file = self.headdir + "4XMM_DR13cat_v1.0.fits"
-        #self.sas_prefix = "/c1/apps/apptainer/1.3.0/bin/apptainer exec --bind /CCAS/groups --bind /lustre /c1/apps/xmmsas/xmmsas"
         self.sas_prefix = ""
